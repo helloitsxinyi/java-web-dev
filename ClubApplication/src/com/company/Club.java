ac587beb42c8fbe0522d7e943d94bb8f9f0acaab
Xinyi <xinzwerkz@gmail.com>
2021-11-30 15:18:11 +0800

    update addMember method to use arrayList instead of array

--- ClubApplication/src/com/company/Club.java
+++ ClubApplication/src/com/company/Club.java
@@ -1,13 +1,15 @@
 package com.company;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Club {
     private int membershipNumber = 0 ;
 
-    // declare and instantiate null array (cos using member objects)
-    private Member[] members = new Member[0];
+    //    // declare and instantiate null array (cos using member objects)
+//    private Member[] members = new Member[0];
+    private List<Member> members = new ArrayList<>();
 
     public Club() {
     }
@@ -20,34 +22,41 @@ public class Club {
         }
     }
 
-    public void addMember(String surname, String firstName, String secondName) {
-        membershipNumber += 1;
-        Member m = new Member(surname, firstName, secondName, membershipNumber);
-        // now, array size may change due to removeMember.
-        // therefore important to assign new length as length of previous members array + 1,
-        // instead of just assigning the membership number.
-        members = Arrays.copyOf(members, members.length+1);
-        members[members.length-1] = m;
-    }
-
-    public void removeMember(int membershipNumber) {
-        int memNo = 0;
-        // to get the index of member to be removed,
-        // as this always changes after addition/deletion
-        for (int i = 0; i < members.length; i++) {
-            if (members[i].getMemberNumber() == membershipNumber) {
-                memNo = i;
-            }
-        }
-        for (int i = memNo; i < members.length - 1; i++) {
-            members[i] = members[i+1];
-        }
-        members = Arrays.copyOf(members, members.length - 1);
-    }
-
-    public Member[] getMembers() {
+//    public void addMember(String surname, String firstName, String secondName) {
+//        membershipNumber += 1;
+//        Member m = new Member(surname, firstName, secondName, membershipNumber);
+//        // now, array size may change due to removeMember.
+//        // therefore important to assign new length as length of previous members array + 1,
+//        // instead of just assigning the membership number.
+//        members = Arrays.copyOf(members, members.length+1);
+//        members[members.length-1] = m;
+//    }
+//
+//    public void removeMember(int membershipNumber) {
+//        int memNo = 0;
+//        // to get the index of member to be removed,
+//        // as this always changes after addition/deletion
+//        for (int i = 0; i < members.length; i++) {
+//            if (members[i].getMemberNumber() == membershipNumber) {
+//                memNo = i;
+//            }
+//        }
+//        for (int i = memNo; i < members.length - 1; i++) {
+//            members[i] = members[i+1];
+//        }
+//        members = Arrays.copyOf(members, members.length - 1);
+//    }
+//
+//    public Member[] getMembers() {
+//        return members;
+//    }
+    public List<Member> getMembers() {
         return members;
     }
 
+    public void addMember(String surname, String firstName, String secondName) {
+        membershipNumber += 1;
+        members.add(new Member(surname, firstName, secondName, membershipNumber));
+    }
 
 }
