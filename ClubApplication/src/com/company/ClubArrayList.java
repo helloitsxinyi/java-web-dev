fa875ebc0a1b9c1d7845af63687af5771a618a39
Xinyi <xinzwerkz@gmail.com>
2021-11-30 15:39:37 +0800

    split club into specific classes with naming to show data types

---
+++ ClubApplication/src/com/company/ClubArrayList.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClubArrayList {
+    private int membershipNumber = 0 ;
+
+    private List<Member> members = new ArrayList<>();
+
+    public ClubArrayList() {
+    }
+
+    public void showMembers() {
+        // in 3rd element will encounter NPE because array is initialized with null objects
+        // and 3rd element is null (only 1st and 2nd filled)
+        for (Member mem : members) {
+            mem.show();
+        }
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void addMember(String surname, String firstName, String secondName) {
+        membershipNumber += 1;
+        members.add(new Member(surname, firstName, secondName, membershipNumber));
+    }
+
+    public void removeMember(int membershipNumber) {
+//        //simple iterator method
+//        members.removeIf(member -> member.getMemberNumber() == membershipNumber);
+        int index = members.indexOf(findMember(membershipNumber));
+        members.remove(index);
+    }
+
+    public Member findMember(int membershipNumber) {
+        for (Member mem: members) {
+            if (mem.getMemberNumber() == membershipNumber) {
+                return mem;
+            }
+        }
+        return null;
+    }
+}
