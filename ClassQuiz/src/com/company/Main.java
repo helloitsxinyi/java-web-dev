8f14dcceaabb6461d44fade3c8002a3bfb326372
Xinyi <xinzwerkz@gmail.com>
2021-12-16 13:52:01 +0800

    add sumItems using generics

--- ClassQuiz/src/com/company/Main.java
+++ ClassQuiz/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    static <T extends Number> double sumItems(List<T> list) {
+        double sum = 0;
+        for (T item : list) {
+            sum += item.doubleValue();
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = new ArrayList<>(); intList.add(1);
+        intList.add(2);
+        intList.add(3); System.out.println(sumItems(intList));
+        List<Double> doubleList = new ArrayList<>(); doubleList.add(1.1);
+        doubleList.add(2.2);
+        doubleList.add(3.3); System.out.println(sumItems(doubleList));
     }
 }
