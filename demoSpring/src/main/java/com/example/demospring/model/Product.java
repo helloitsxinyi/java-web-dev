fcb480a9fe590e379fc074fdfd8699edcd230ae0
Xinyi <xinzwerkz@gmail.com>
2021-12-21 20:09:52 +0800

    add Product class

---
+++ demoSpring/src/main/java/com/example/demospring/model/Product.java
@@ -0,0 +1,63 @@
+package com.example.demospring.model;
+
+import java.util.Objects;
+
+public class Product {
+    private Integer productId;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId.equals(product.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
