746e84c500939143600975c37d03421c6cb6b2e1
Xinyi <xinzwerkz@gmail.com>
2021-12-23 23:51:16 +0800

    add student class to show lombok usage

---
+++ demoSpring/src/main/java/com/example/demospring/model/Student.java
@@ -0,0 +1,10 @@
+package com.example.demospring.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// using @Data, lombok helps to generate toString, equalsHashCode, getter, setter, requiredArgsConstructor.
+@Data
+@NoArgsConstructor
+public class Student {
+}
