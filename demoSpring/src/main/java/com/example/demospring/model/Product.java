d1d51eb35eb1e8e7fb997dff448e385306a0740c
Xinyi <xinzwerkz@gmail.com>
2021-12-24 11:47:11 +0800

    add lombok for product

--- demoSpring/src/main/java/com/example/demospring/model/Product.java
+++ demoSpring/src/main/java/com/example/demospring/model/Product.java
@@ -1,10 +1,15 @@
 package com.example.demospring.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name="product")
+@Data
+@NoArgsConstructor
 public class Product {
     // generation strategy is a MUST (identifier)
     // IDENTITY strategy gives control to connectivity framework(JPA). JPA designs.
@@ -24,54 +29,5 @@ public class Product {
         this.price = price;
     }
 
-    public Product() {
 
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return productId.equals(product.productId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(productId);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "productId=" + productId +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }
