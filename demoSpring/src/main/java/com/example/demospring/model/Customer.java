a6c095f5150f450a6a232a59b729778830127d0d
Xinyi <xinzwerkz@gmail.com>
2021-12-24 11:46:21 +0800

    use lombok for customer

--- demoSpring/src/main/java/com/example/demospring/model/Customer.java
+++ demoSpring/src/main/java/com/example/demospring/model/Customer.java
@@ -1,10 +1,15 @@
 package com.example.demospring.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name="customer")
+@Data
+@NoArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,44 +22,5 @@ public class Customer {
         this.address = address;
     }
 
-    public Customer() {
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return customerId.equals(customer.customerId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(customerId);
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
