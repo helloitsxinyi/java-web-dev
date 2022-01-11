4b45e34258329aef5f14cb0c286e2472c58ca5e6
Xinyi <xinzwerkz@gmail.com>
2022-01-11 20:24:07 +0800

    add sillyController with comments and index page

---
+++ demoSpring/src/main/java/com/example/demospring/controller/SillyController.java
@@ -0,0 +1,37 @@
+package com.example.demospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+// ReqMapping is a more general form of specific HTTP mappings.
+// use either Controller or REST controller
+// use RestController if you want endpoints to be generated
+// RestController for fullstack app (eg ReactJS), Controller for traditional app (eg Thymeleaf)
+@Controller
+@RequestMapping("/first")
+public class SillyController {
+	
+	@PostMapping("/second")
+	public String method1() {
+		return "index";
+	} 
+
+	@GetMapping("/third")
+	public String method2(Model model) {
+		String message = "Some silly funny message";
+		model.addAttribute("s", message);
+		return "index";
+	}
+	
+	// alt to GetMapping
+	@RequestMapping(value = "/fourth", method = RequestMethod.GET) 
+	public String method4() {
+		return "index";
+		
+		
+	}
+}
