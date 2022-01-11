d3cec51926d54bb8c3b1e48b1a7a5b198ebbbd29
Xinyi <xinzwerkz@gmail.com>
2022-01-11 20:42:07 +0800

    add atest template and method5 to SillyController

--- demoSpring/src/main/java/com/example/demospring/controller/SillyController.java
+++ demoSpring/src/main/java/com/example/demospring/controller/SillyController.java
@@ -3,9 +3,12 @@ package com.example.demospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demospring.model.Address;
 
 // ReqMapping is a more general form of specific HTTP mappings.
 // use either Controller or REST controller
@@ -31,7 +34,14 @@ public class SillyController {
 	@RequestMapping(value = "/fourth", method = RequestMethod.GET) 
 	public String method4() {
 		return "index";
-		
-		
+	}
+	
+	// alt to model.addAttribute (see "/third")
+	@GetMapping("/fifth")
+	public String method5(@ModelAttribute("a") Address add) {
+		add.setStreet("street");
+		add.setCity("SG");
+		add.setCountry("Singapore");
+		return "atest";
 	}
 }
