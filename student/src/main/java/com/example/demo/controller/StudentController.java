e32f57223ce5802de3a34380c9a951dbd68d68a2
Xinyi <xinzwerkz@gmail.com>
2022-01-16 21:48:07 +0800

    add post mapping for addstudent

--- student/src/main/java/com/example/demo/controller/StudentController.java
+++ student/src/main/java/com/example/demo/controller/StudentController.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.Student;
@@ -25,5 +27,23 @@ public class StudentController {
 		model.addAttribute("students", slist);
 		return "slisting";
 	}
+	
+	// whenever we go to /load, controller creates an empty instance student,
+	// attaches it to Model, then goes to sform.
+	@RequestMapping("/load")
+	public String loadStudentForm(Model model) {
+		Student student = new Student();
+		// 1st arg-student object, 2nd arg-pass in empty data which is student instance.
+		model.addAttribute("student", student);
+		return "sform";
+	}
+	
+	@PostMapping("/addstudent")
+	public String addStudent(@ModelAttribute("student") Student student, Model model) {
+		srepo.saveAndFlush(student);
+		// go back to listing page
+		return "all";
+	}
+	
 
 }
