b6d155e53ae9486fddb5e6e3d67f9e104ef4a460
Xin Yi Tay <xinzwerkz@gmail.com>
2022-06-06 14:42:02 +0800

    add spring workshop files

---
+++ 2022/demo/src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class Customer {
+        private String id;
+        private String name;
+        public Customer(String id, String name) {
+            super();
+            this.id = id;
+            this.name = name;
+        }
+        public Customer() {
+            super();
+// TODO Auto-generated constructor stub }
+// Other constructors, Getters, Setters and toString() ...
+        }
+}
