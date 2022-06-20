b8b5d0c6bb710f39c18fd83c1d8a66f6fdd42124
Xin Yi Tay <xinzwerkz@gmail.com>
2022-06-20 12:02:12 +0800

    add user properties in prep for login and logout

--- 2022/controllerdemo/src/main/java/com/example/controllerdemo/model/User.java
+++ 2022/controllerdemo/src/main/java/com/example/controllerdemo/model/User.java
@@ -1,4 +1,22 @@
 package com.example.controllerdemo.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
 }
